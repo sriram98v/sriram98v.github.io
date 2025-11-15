1ac61aac3c007231e40d53004849aed3e9845747
sriram98v <vijendran.sriram@gmail.com>
2025-11-15 13:41:29 -0600

    updated 2025115

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A flake for developing and building my personal website";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.website = pkgs.stdenv.mkDerivation rec {
+          pname = "static-website";
+          version = "2021-11-19";
+          src = ./.;
+          nativeBuildInputs = [pkgs.zola];
+          buildPhase = "zola build";
+          installPhase = "cp -r public $out";
+        };
+        defaultPackage = self.packages.${system}.website;
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            zola
+          ];
+        };
+      }
+    );
+}
